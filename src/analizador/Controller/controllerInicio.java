645f8d8091a6a1e3bc23698b4da435e32cd50946
Uziel <201274@ids.upchiapas.edu.mx>
2022-06-27 08:39:14 -0500

    Vista bien diseñado y separacion de los datos obtenidos listo desde controller

--- src/analizador/Controller/controllerInicio.java
+++ src/analizador/Controller/controllerInicio.java
@@ -1,5 +1,45 @@
 package analizador.Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+import java.util.StringTokenizer;
+
 public class controllerInicio {
+    ObservableList<String> entrada = FXCollections.observableArrayList();
+
+    @FXML
+    private TextArea classMain;
+
+    @FXML
+    private TextArea classObjet;
+
+    @FXML
+    void iniciarAnalizis(ActionEvent event) {
+
+        StringTokenizer res = new StringTokenizer(classObjet.getText());
+        while (res.hasMoreTokens()) {
+            //System.out.println(st.nextToken());
+            entrada.add(res.nextToken());
+        }
+
+        StringTokenizer st = new StringTokenizer(classMain.getText());
+        while (st.hasMoreTokens()) {
+            //System.out.println(st.nextToken());
+            entrada.add(st.nextToken());
+        }
+
+
+        ImprimirArray();
+    }
+
+    public void ImprimirArray(){
+        for (String s : entrada) {
+            System.out.println(s);
+        }
+    }
 
 }
